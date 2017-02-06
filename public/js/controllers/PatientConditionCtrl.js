670bfe6eb734c7357c25cffd05668e3db226cf77
Benjamin <benji015@hotmail.com>
2017-02-06 16:13:16 -0500

    Added folder structure

---
+++ public/js/controllers/PatientConditionCtrl.js
@@ -0,0 +1,9 @@
+angular.module('PatientConditionCtrl', []).controller('PatientConditionController', function($scope) {
+
+	// Données temporaires pour le front end
+	$scope.title = 'État des patients';
+	$scope.patients = [
+		{firstName: 'Roy', lastName: 'Benjamin', age: 78, condition: 'stable'},
+		{firstName: 'Tran', lastName: 'Austin-Didier', age: 78, condition: 'instable'},
+	];
+});
