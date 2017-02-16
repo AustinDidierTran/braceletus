3c8954f60df256425cc9b9f7df8705a48c3647b9
Benjamin Roy <benji015@hotmail.com>
2017-02-16 11:55:57 -0500

    Added firebase config (#5)

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBKjNO1GcxwDVylqAizWQgMKK3NeOGGJO0",
+    authDomain: "braceletus-7a2e6.firebaseapp.com",
+    databaseURL: "https://braceletus-7a2e6.firebaseio.com",
+    storageBucket: "braceletus-7a2e6.appspot.com",
+    messagingSenderId: "90223967938"
+};
+firebase.initializeApp(config);
+
 angular.module('braceletusApp', [
     'ngRoute',
     'appRoutes',
