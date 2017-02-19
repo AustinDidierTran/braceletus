c92d4e930ab5302a331628e12b8173fafd36407b
Benjamin <benji015@hotmail.com>
2017-02-19 14:10:22 -0500

    This closes #8

--- app/public/js/controllers/PatientInformationCtrl.js
+++ app/public/js/controllers/PatientInformationCtrl.js
@@ -6,9 +6,9 @@ angular.module('PatientInformationCtrl', []).controller('PatientInformationContr
   // The user should be retrieved with the id from firebase
 	$scope.patient = {
     id: $route.current.params.id,
-    firstName: 'Roy', 
-    lastName: 'Benjamin', 
-    age: 78, 
+    firstName: 'Roy',
+    lastName: 'Benjamin',
+    age: 78,
     condition: 'stable'
   };
 });
--- app/public/js/controllers/PatientsListCtrl.js
+++ app/public/js/controllers/PatientsListCtrl.js
@@ -1,41 +1,67 @@
-angular.module('PatientsListCtrl', []).controller('PatientsListController', function($scope, DTOptionsBuilder, DTColumnBuilder) {
+angular.module('PatientsListCtrl', []).controller('PatientsListController',
+function($compile, $location, $scope, DTOptionsBuilder, DTColumnBuilder) {
 
 	$scope.title = 'État des patients';
 	$scope.dtInstance = {};
 	$scope.dtOptions = DTOptionsBuilder.newOptions()
 		.withOption('data', [
-			{id: 1, picture: null, firstName: 'Roy', lastName: 'Benjamin', age: 78, isStable: true},
-			{id: 2, picture: null, firstName: 'Tran', lastName: 'Austin-Didier', age: 78, isStable: false},
+			{id: 1, picture: null, firstName: 'Roy', lastName: 'Benjamin', age: 78, sex: 'Homme', isStable: true},
+			{id: 2, picture: null, firstName: 'Tran', lastName: 'Austin-Didier', age: 78, sex: 'Femme', isStable: false},
 		])
 		.withPaginationType('full_numbers')
-		.withOption('rowCallback', rowCallback);
+		.withOption('rowCallback', rowCallback)
+		.withOption('createdRow', createdRow)
+		.withBootstrap();
 
 	$scope.dtColumns = [
 		DTColumnBuilder.newColumn('id').withTitle('ID'),
-		DTColumnBuilder.newColumn('picture').withTitle('Photo'),
+		DTColumnBuilder.newColumn('picture').withTitle('Photo').renderWith(setProfilPicture),
 		DTColumnBuilder.newColumn('firstName').withTitle('Prénom'),
 		DTColumnBuilder.newColumn('lastName').withTitle('Nom'),
-		DTColumnBuilder.newColumn('isStable').withTitle('Est stable'),
-		DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable().renderWith(actionsHtml)
+		DTColumnBuilder.newColumn('age').withTitle('Âge'),
+		DTColumnBuilder.newColumn('sex').withTitle('Sexe'),
+		//DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable().renderWith(actionsHtml)
 	];
 
-	function actionsHtml(data, type, full, meta) {
+	$scope.addPatient = function() { console.log('Add patient'); }
+
+	function createdRow(row, data, dataIndex) {
+		// Recompiling so we can bind Angular directive
+		if (!data.isStable) {
+			$('td', row).css('background-color', '#FFCDD2');
+		}
+		$compile(angular.element(row).contents())($scope);
+	}
+
+	function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+		// Unbind first in order to avoid any duplicate handler
+		$('td', nRow).unbind('click');
+		$('td', nRow).bind('click', function() {
+			$location.path('/patient/' + aData.id);
+			$scope.$apply();
+		});
+		return nRow;
+	}
+
+	/*function actionsHtml(data, type, full, meta) {
 		return '<button class="btn btn-warning" ng-click="">' +
 			'   <i class="fa fa-edit"></i>' +
 			'</button>&nbsp;' +
 			'<button class="btn btn-danger" ng-click="" )"="">' +
 			'   <i class="fa fa-trash-o"></i>' +
 			'</button>';
-	}
+	}*/
 
-	/**** This function will be used to redirect the user to the patient information view ***/
-	function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-		// Unbind first in order to avoid any duplicate handler
-		$('td', nRow).unbind('click');
-		$('td', nRow).bind('click', function() {
-				console.log('Row has been clicked');
-		});
-		return nRow;
-	}
+	function setProfilPicture(data, type, full, meta) {
+		var imagePath;
+		if (data === null && full.sex === 'Femme') {
+			imagePath = '../../img/female-picture.png';
+		} else if (data === null && full.sex === 'Homme') {
+			imagePath = '../../img/male-picture.png';
+		} else {
+			// TODO: Fetch profil picture from database.
 
+		}
+		return '<img src=' + imagePath + '>'
+	}
 });
