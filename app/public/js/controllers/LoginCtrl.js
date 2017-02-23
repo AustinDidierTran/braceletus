7f6e70fa52758e403164878800f1e3b7d0102b14
Benjamin Roy <benji015@hotmail.com>
2017-02-23 15:26:32 -0500

    This closes #16 (#26)

--- app/public/js/controllers/LoginCtrl.js
+++ app/public/js/controllers/LoginCtrl.js
@@ -1,15 +1,29 @@
-angular.module('LoginCtrl', []).controller('LoginController', function($scope) {
-  $scope.username = '';
-  $scope.password = '';
+var app = angular.module('LoginCtrl', []);
 
-  $scope.connect = function() {
-    console.log('Username: ' + $scope.username + ' , Password: ' + $scope.password);
-  }
-  $scope.forgotPassword = function() {
-    console.log('Forgot password...');
-  }
-  $scope.register = function() {
-    console.log('Register...');
+app.controller('LoginController', function($location, $rootScope, $scope, $timeout, firebaseService) {
+
+  // TODO: Replace alerts by ui-bootstrap-alerts: https://angular-ui.github.io/bootstrap/
+  $scope.user = { email: '', password: '' };
+
+  $scope.resetPassword = function() {
+    firebaseService.resetPassword($scope.user.email).then(function() {
+      alert('Un courriel de réinitialisation du mot de passe a été envoyé à l\'addresse ' + $scope.user.email);
+    }, function(error) {
+      alert('Impossible d\'envoyer un courriel à l\'addresse suivante: ' + $scope.user.email);
+    });
   }
 
+  $scope.signIn = function() {
+    firebaseService.signIn($scope.user.email, $scope.user.password).then(function(user) {
+      $rootScope.user = user; // TODO: Store the user in cache?
+      $location.path('/patients');
+      $scope.$apply();
+    }).catch(function(error) {
+      console.log(error.code + ': ' + error.message);
+      alert('Le nom d\'utilisateur ou le mot de passe est incorrect');
+    });
+  }
+
+  $scope.signUp = function() {}
+  
 });
