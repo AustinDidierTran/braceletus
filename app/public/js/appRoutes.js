46852cafeb8389c3cb97b762d8b528535bfd74b9
marc1119 <marc1119@live.ca>
2017-02-28 08:38:10 -0500

    Ajout de error 404, du dossier du patient et de la page pour créer/modifier un dossier de patient

--- app/public/js/appRoutes.js
+++ app/public/js/appRoutes.js
@@ -30,6 +30,14 @@ app.config(['$locationProvider', '$routeProvider',
         }]
       }
     })
+    .when('/error', {
+      templateUrl: 'views/error404.html',
+      controller: 'PatientInformationController'
+    })
+    .when('/patientForm', {
+      templateUrl: 'views/patient-form.html',
+      controller: 'PatientInformationController'
+    })
     .otherwise({redirectTo:'/login'});
 
     $locationProvider.html5Mode(true);
---
+++ app/public/js/controllers/PatientFormCtrl.js
@@ -0,0 +1,14 @@
+angular.module('PatientInformationCtrl', []).controller('PatientInformationController', function($scope, $route) {
+
+	// Temporary data for the front-end
+	$scope.title = 'État des patients';
+
+  // The user should be retrieved with the id from firebase
+	$scope.patient = {
+    id: $route.current.params.id,
+    firstName: 'Roy',
+    lastName: 'Benjamin',
+    age: 78,
+    condition: 'stable'
+  };
+});
