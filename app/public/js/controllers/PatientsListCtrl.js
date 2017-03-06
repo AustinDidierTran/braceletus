3181a238127e4c79ccf1207419b9bca27f01f19e
Benjamin Roy <benji015@hotmail.com>
2017-03-06 15:08:21 -0500

    This closes #25 (#29)

--- app/public/js/controllers/PatientsListCtrl.js
+++ app/public/js/controllers/PatientsListCtrl.js
@@ -1,67 +1,36 @@
-angular.module('PatientsListCtrl', []).controller('PatientsListController',
-function($compile, $location, $scope, DTOptionsBuilder, DTColumnBuilder) {
+var app = angular.module('PatientsListCtrl', []);
 
-	$scope.title = 'État des patients';
-	$scope.dtInstance = {};
-	$scope.dtOptions = DTOptionsBuilder.newOptions()
-		.withOption('data', [
-			{id: 1, picture: null, firstName: 'Roy', lastName: 'Benjamin', age: 78, sex: 'Homme', isStable: true},
-			{id: 2, picture: null, firstName: 'Tran', lastName: 'Austin-Didier', age: 78, sex: 'Femme', isStable: false},
-		])
-		.withPaginationType('full_numbers')
-		.withOption('rowCallback', rowCallback)
-		.withOption('createdRow', createdRow)
-		.withBootstrap();
+app.controller('PatientsListController',
+	function($location, $scope, DTOptionsBuilder, DTColumnDefBuilder, firebaseService) {
 
-	$scope.dtColumns = [
-		DTColumnBuilder.newColumn('id').withTitle('ID'),
-		DTColumnBuilder.newColumn('picture').withTitle('Photo').renderWith(setProfilPicture),
-		DTColumnBuilder.newColumn('firstName').withTitle('Prénom'),
-		DTColumnBuilder.newColumn('lastName').withTitle('Nom'),
-		DTColumnBuilder.newColumn('age').withTitle('Âge'),
-		DTColumnBuilder.newColumn('sex').withTitle('Sexe'),
-		//DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable().renderWith(actionsHtml)
-	];
+		$scope.title = 'État des patients';
+		$scope.patients = firebaseService.getAll('patients');
+		$scope.dtInstance = {};
+		$scope.dtOptions = DTOptionsBuilder.newOptions()
+			.withPaginationType('full_numbers')
+			.withOption('rowCallback', rowCallback)
+			.withOption('order', [])
+			.withBootstrap();
+		$scope.dtColumnDefs = [
+			DTColumnDefBuilder.newColumnDef(0).notSortable(), // ID
+			DTColumnDefBuilder.newColumnDef(1).notSortable(), // Picture
+			DTColumnDefBuilder.newColumnDef(2).notSortable(), // First Name
+			DTColumnDefBuilder.newColumnDef(3).notSortable(), // Last Name
+			DTColumnDefBuilder.newColumnDef(4).notSortable(),	// Age
+			DTColumnDefBuilder.newColumnDef(5).notSortable(), // Sex
+			DTColumnDefBuilder.newColumnDef(6).notSortable() // Condition
+	  ];
 
-	$scope.addPatient = function() { console.log('Add patient'); }
+		$scope.addPatient = function() { console.log('Add patient'); }
 
-	function createdRow(row, data, dataIndex) {
-		// Recompiling so we can bind Angular directive
-		if (!data.isStable) {
-			$('td', row).css('background-color', '#FFCDD2');
+		function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+			// Unbind first in order to avoid any duplicate handler
+			$('td', nRow).unbind('click');
+			$('td', nRow).bind('click', function() {
+				$location.path('/patient/' + aData[0]);
+				$scope.$apply();
+			});
+			return nRow;
 		}
-		$compile(angular.element(row).contents())($scope);
-	}
 
-	function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-		// Unbind first in order to avoid any duplicate handler
-		$('td', nRow).unbind('click');
-		$('td', nRow).bind('click', function() {
-			$location.path('/patient/' + aData.id);
-			$scope.$apply();
-		});
-		return nRow;
-	}
-
-	/*function actionsHtml(data, type, full, meta) {
-		return '<button class="btn btn-warning" ng-click="">' +
-			'   <i class="fa fa-edit"></i>' +
-			'</button>&nbsp;' +
-			'<button class="btn btn-danger" ng-click="" )"="">' +
-			'   <i class="fa fa-trash-o"></i>' +
-			'</button>';
-	}*/
-
-	function setProfilPicture(data, type, full, meta) {
-		var imagePath;
-		if (data === null && full.sex === 'Femme') {
-			imagePath = '../../img/female-picture.png';
-		} else if (data === null && full.sex === 'Homme') {
-			imagePath = '../../img/male-picture.png';
-		} else {
-			// TODO: Fetch profil picture from database.
-
-		}
-		return '<img src=' + imagePath + '>'
-	}
 });
--- app/public/js/services/firebaseService.js
+++ app/public/js/services/firebaseService.js
@@ -1,4 +1,14 @@
-angular.module('FirebaseService', []).service('firebaseService', function($rootScope) {
+angular.module('FirebaseService', []).service('firebaseService', function($firebaseArray) {
+
+  this.getAll = function(key) {
+    var ref = firebase.database().ref().child(key);
+    return $firebaseArray(ref);
+  }
+
+  this.getImageFullPath = function(key) {
+    var storageRef = firebase.storage().ref();
+    return storageRef.child(key).getDownloadURL();
+  }
 
   this.setUserPassword = function(newPassword) {
     var user = firebase.auth().currentUser; // The user is using Firebase
