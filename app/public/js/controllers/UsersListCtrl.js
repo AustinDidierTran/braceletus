124ebd6360d09194252d4a82f50ebd85f47b1f5c
Austin-Didier Tran <tran.austin.didier@gmail.com>
2017-04-10 13:20:52 -0400

    User list update (#40)
    * Working on profiles
    
    * Now creates profiles on login and signup
    
    * Updated profile creation and user list

--- app/public/js/controllers/UsersListCtrl.js
+++ app/public/js/controllers/UsersListCtrl.js
@@ -7,22 +7,7 @@ app.controller('UsersListController',
 
     $scope.title = 'Liste des utilisateurs';
     $scope.types = [ 'Administrateur', 'Utilisateur' ];
-    $scope.users = [
-      {
-        email: 'austin.didier.tran@gmail.com',
-        picture: 'https://firebasestorage.googleapis.com/v0/b/braceletus-7a2e6.appspot.com/o/images%2Fusers%2Faustindidiertran.jpg?alt=media&token=c50fc365-8e81-41e0-9076-d6d4839ce28c',
-        firstName: 'Austin-Didier',
-        lastName: 'Tran',
-        type: 'Utilisateur'
-      },
-      {
-        email: 'benji015@hotmail.com',
-        picture: 'https://firebasestorage.googleapis.com/v0/b/braceletus-7a2e6.appspot.com/o/images%2Fusers%2Fbenjaminroy.jpg?alt=media&token=58178873-83ee-408f-b8b5-ae8914431222',
-        firstName: 'Benjamin',
-        lastName: 'Roy',
-        type: 'Administrateur'
-      }
-    ];
+    $scope.users = firebaseService.getUsers();
   	$scope.dtInstance = {};
     $scope.dtOptions = DTOptionsBuilder.newOptions()
       .withPaginationType('full_numbers')
